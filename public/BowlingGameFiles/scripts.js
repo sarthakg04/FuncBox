60ddabf6266b47f590526c3708b0ea8cdb60d04e
Sarthakg04 <sarthakanamika@gmail.com>
2021-09-02 18:43:03 +0530

    changes

--- public/BowlingGameFiles/scripts.js
+++ public/BowlingGameFiles/scripts.js
@@ -1,5 +1,4 @@
 function canvas(){
-
 var canvas = document.createElement('canvas');
 canvas.id = "CursorLayer";
 canvas.style.zIndex = 8;
@@ -10,5 +9,22 @@ body.appendChild(canvas);
 cursorLayer = document.getElementById("CursorLayer");
 console.log(cursorLayer);
 document.getElementById("CursorLayer").style.backgroundImage='url(FuncBoxx/BowlingGameFiles/assets/backgrounds/Frame.png)';
-return canvas;
+}
+
+function addball(){
+var canvas = document.getElementById('CursorLayer');
+var context = canvas.getContext('2d');
+var centerX = canvas.width / 2;
+var centerY = canvas.height / 2;
+var radius = 70;
+
+let circle = new Path2D();  // <<< Declaration
+circle.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+
+context.fillStyle = 'blue';
+context.fill(circle); //   <<< pass circle to context
+
+context.lineWidth = 10;
+context.strokeStyle = '#000066';
+context.stroke(circle); 
 }
