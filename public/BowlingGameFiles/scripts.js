8b6dae89c54a05a3a48d3e91c9b4f515e032747d
Sarthakg04 <sarthakanamika@gmail.com>
2021-09-05 20:50:28 +0530

    Animation added to Bowling game

--- public/BowlingGameFiles/scripts.js
+++ public/BowlingGameFiles/scripts.js
@@ -1,4 +1,5 @@
 // Frontend Function
+let flag=1;
 function createGamepad() {
     let gamePadd = document.createElement('DIV')
     gamePadd.classList.add('GamePad')
@@ -12,38 +13,57 @@ function fill(background) {
 }
 function createInteractionPad(){
   gamePad.innerHTML += `
-  <img class="ballstyle" id="ball" src="FuncBoxx/BowlingGameFiles/assets/images/ball.png" height="50vh" width="50vw">
+  <div class="ball1" id="ball">
+  <img class="ballstyle"src="FuncBoxx/BowlingGameFiles/assets/images/ball.png" height="50vh" width="50vw"/>
+  </div>
+  <div class="buttons" id="controllers">
+  <button onclick="control(0)" >Play</button>
+  <button onclick="control(1); flag=0">Shoot</button>
+  </div>
   `;
+  
 }
-function up(){
+function control(value){
+  if (value==0){
   let id = null;
-  const elem = document.getElementById('ball');
+  let elem = document.getElementById('ball');
   const dem = elem.getBoundingClientRect();  
-  let pos = dem.top;
+  let post = 0;
+  clearInterval(id);
+  let i;
+  for(i=0;i<3;i++){
+  id = setInterval(frame, 50);
+  }
+  function frame() {
+    if (post == 350) {
+      clearInterval(id);
+      post=0;
+    } 
+    else if(flag==0){
+      clearInterval(id);
+      post=0;
+    }
+    else {
+      post++; 
+      elem.style.marginLeft = post + "px"; 
+    }
+  }}
+  else{
+  let id = null;
+  let elem = document.getElementById('ball');
+  const dem = elem.getBoundingClientRect();  
+  let pos = dem.top | 0;
   clearInterval(id);
   id = setInterval(frame, 2);
   function frame() {
-    if (pos == 0) {
-    } else {
+    if (pos == -400) {
+      clearInterval(id);  
+    } 
+    else {
+      console.log(pos);
       pos--; 
       elem.style.marginTop = pos + "px"; 
     }
   }
-}
-function left(){
-  let id = null;
-  const elem = document.getElementById('ball');
-  const dem = elem.getBoundingClientRect();  
-  let post = 0;
-  clearInterval(id);
-  id = setInterval(frame, 5);
-  function frame() {
-    if (post == 350) {
-      clearInterval(id);
-    } else {
-      console.log("are");
-      post++; 
-      elem.style.marginLeft = post + "px"; 
-    }
   }
 }
