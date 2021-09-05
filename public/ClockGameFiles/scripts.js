a6dfe5f5bae924e4dd6f97964a5a6bf48f779ab0
Sarthakg04 <sarthakanamika@gmail.com>
2021-09-06 03:53:53 +0530

    Added Changes in ClockGame

--- public/ClockGameFiles/scripts.js
+++ public/ClockGameFiles/scripts.js
@@ -1,3 +1,4 @@
+let clock =['02:00 PM','04:00 AM'];
 function createGamepad() {
     let gamePadd = document.createElement('DIV')
     gamePadd.classList.add('GamePad')
@@ -14,10 +15,8 @@ function createInteractionPad(){
   Guess
   </div>
   <div class="game">
-  <img class="clock" src="FuncBoxx/ClockGameFiles/assets/images/2_45_PM.png" height="350vh" width="270vw">
-  <div class="inputfield">
-  10:00 AM
-  </div>
+  <img class="clock" src="FuncBoxx/ClockGameFiles/assets/images/2_45_PM.png" height="350vh" width="270vw">  
+  <input class="inputfield" type="text" placeholder="10:00 AM" style="outline-width:0;">
   <div class="controllers" id="control">
   <button class="submitbutton" onclick="control(0)" >Submit</button>
   <button class="reloadbutton" onclick="control(1)">Reload</button>
