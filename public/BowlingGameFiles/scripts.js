8092311f93c78432c99dab0c54b2f2f50a76ddc8
Sarthakg04 <sarthakanamika@gmail.com>
2021-09-06 02:16:57 +0530

    Final Game

--- public/BowlingGameFiles/scripts.js
+++ public/BowlingGameFiles/scripts.js
@@ -1,4 +1,3 @@
-// Frontend Function
 let flag=1;
 let lpos=0;
 function createGamepad() {
@@ -7,15 +6,27 @@ function createGamepad() {
     gamePadd.id = 'GamePad'
     document.body.appendChild(gamePadd);
     gamePad = document.getElementById('GamePad')
-    // return gamePad;
 }
 function fill(background) {
   document.getElementsByClassName('GamePad')[0].style.backgroundImage =  'url(FuncBoxx/BowlingGameFiles/assets/backgrounds/Frame.png)';
 }
 function createInteractionPad(){
   gamePad.innerHTML += `
+  <div class="score" id="score">
+  0/9
+  </div>
   <div class="game">
-  <img  id="ball" src="FuncBoxx/BowlingGameFiles/assets/images/ball.png" height="50vh" width="50vw"/>
+  
+  <img  id="ball" src="FuncBoxx/BowlingGameFiles/assets/images/ball.png" class="pin">
+  <img id="pin1" src="FuncBoxx/BowlingGameFiles/assets/images/pin.png" class="pin" >
+  <img id="pin2" src="FuncBoxx/BowlingGameFiles/assets/images/pin.png" class="pin">
+  <img id="pin3" src="FuncBoxx/BowlingGameFiles/assets/images/pin.png" class="pin">
+  <img id="pin4" src="FuncBoxx/BowlingGameFiles/assets/images/pin.png" class="pin">
+  <img id="pin5" src="FuncBoxx/BowlingGameFiles/assets/images/pin.png" class="pin">
+  <img id="pin6" src="FuncBoxx/BowlingGameFiles/assets/images/pin.png" class="pin">
+  <img id="pin7" src="FuncBoxx/BowlingGameFiles/assets/images/pin.png" class="pin">
+  <img id="pin8" src="FuncBoxx/BowlingGameFiles/assets/images/pin.png" class="pin">
+  <img id="pin9" src="FuncBoxx/BowlingGameFiles/assets/images/pin.png" class="pin">
   <div class="controllers" id="control">
   <button onclick="control(0)" >Play</button>
   <button onclick="control(1); flag=0">Shoot</button>
@@ -37,13 +48,12 @@ function control(value){
   }
   function frame() {
     if (post == 350) {
-      clearInterval(id);
-      lpos=post;
+      clearInterval(id);  
       post=0;
     }
     else if(flag==0){
+      lpos=post;
       clearInterval(id);
-      post=0;
     }
     else {
       post++; 
@@ -61,7 +71,8 @@ function control(value){
     function frame() {
       if (post == 50) {
         clearInterval(id);
-        post=0;
+        console.log(lpos);
+        result();
       } 
       else {
         post--; 
@@ -70,5 +81,34 @@ function control(value){
     }
 }}
 function result(){
-  
+  if (lpos<110 && lpos>=0)
+  { 
+    document.getElementById("score").innerHTML='3/9';
+    document.getElementById('pin1').style.opacity=0;
+    document.getElementById('pin2').style.opacity=0;
+    document.getElementById('pin3').style.opacity=0;
+    console.log('Left 3 pins')
+  }
+  else if(lpos>=110 && lpos<220)
+  {
+    document.getElementById("score").innerHTML='9/9 You Won';
+    document.getElementById('pin1').style.opacity=0;
+    document.getElementById('pin2').style.opacity=0;
+    document.getElementById('pin3').style.opacity=0;
+    document.getElementById('pin4').style.opacity=0;
+    document.getElementById('pin5').style.opacity=0;
+    document.getElementById('pin6').style.opacity=0;
+    document.getElementById('pin7').style.opacity=0;
+    document.getElementById('pin8').style.opacity=0;
+    document.getElementById('pin9').style.opacity=0;
+    console.log('All pins')
+  }
+  else
+  {
+    document.getElementById("score").innerHTML='3/9';
+    document.getElementById('pin7').style.opacity=0;
+    document.getElementById('pin8').style.opacity=0;
+    document.getElementById('pin9').style.opacity=0;
+    console.log('Right 3 pins')
+  }
 }
