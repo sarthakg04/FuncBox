a1077a14640a3d6d3d4e8f2d4ac8bab58a72db86
Sarthakg04 <sarthakanamika@gmail.com>
2021-09-06 13:49:50 +0530

    Final Clock Game Updated

--- public/ClockGameFiles/scripts.js
+++ public/ClockGameFiles/scripts.js
@@ -1,4 +1,7 @@
-let clock =['02:00 PM','04:00 AM'];
+
+let result_time='02:55 PM';
+let pathofimg='FuncBoxx/ClockGameFiles/assets/images/02_55_PM.png';
+let clock =['02_55_AM','04_00_AM','05_00_AM','10_10_PM','07_10_AM','01_25_PM','07_45_PM','09_35_PM','04_30_PM','11_45_AM'];
 function createGamepad() {
     let gamePadd = document.createElement('DIV')
     gamePadd.classList.add('GamePad')
@@ -12,15 +15,43 @@ function fill(background) {
 function createInteractionPad(){
   gamePad.innerHTML += `
   <div class="score" id="score">
-  Guess
+  Guess the Correct Time!
   </div>
   <div class="game">
-  <img class="clock" src="FuncBoxx/ClockGameFiles/assets/images/2_45_PM.png" height="350vh" width="270vw">  
-  <input class="inputfield" type="text" placeholder="10:00 AM" style="outline-width:0;">
+  <img class="clock" id="clock" src="FuncBoxx/ClockGameFiles/assets/images/02_55_PM.png" height="350vh" width="270vw">  
+  <input class="inputfield" id="inputfield" type="text" placeholder="10:00 AM" style="outline-width:0;">
   <div class="controllers" id="control">
-  <button class="submitbutton" onclick="control(0)" >Submit</button>
-  <button class="reloadbutton" onclick="control(1)">Reload</button>
+  <button class="submitbutton" onclick="result()" >Submit</button>
+  <button class="reloadbutton" onclick="reload()">Reload</button>
   </div>
   </div>
   `;
 }
+function getRandomItem(arr) {
+  const randomIndex = Math.floor(Math.random() * arr.length);
+  const item = arr[randomIndex];
+  return item;
+}
+function reload(){
+  document.getElementById('inputfield').style.backgroundColor ="#C4C4C4";
+  let temp=getRandomItem(clock);
+  let mainpath="FuncBoxx/ClockGameFiles/assets/images/";
+  pathofimg=mainpath+temp+'.png';
+  document.getElementById("clock").src=pathofimg;
+  const myArr = temp.split("_");
+  result_time=myArr[0]+':'+myArr[1]+' '+myArr[2];
+}
+function result(){
+  let inputtime=document.getElementById('inputfield').value;
+  if (result_time==inputtime){
+    console.log('success');
+    document.getElementById('inputfield').style.backgroundColor = "green";
+  }
+  else{
+    console.log(inputtime);
+    console.log(result_time);
+    console.log('Failed');
+    document.getElementById('inputfield').style.backgroundColor = "#D93E3E";
+  }
+}
+
