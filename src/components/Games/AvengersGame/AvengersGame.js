1679ca727b01b66cfa3d605eb80a419897cac1a0
HemanthKumar200225 <hemanthkumarbhamidipati@gmail.com>
2021-09-21 16:50:53 +0530

    AvengersGame Finished🐱‍👤

--- src/components/Games/AvengersGame/AvengersGame.js
+++ src/components/Games/AvengersGame/AvengersGame.js
@@ -16,24 +16,11 @@ export default function AvengersGame() {
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-                <title>MarsRover Game</title>
+                <title>Avengers Game</title>
                 <link rel="stylesheet" href="FuncBoxx/AvengersGameFiles/style.css">
             </head>
 
             <body>
-                <div class="Phone">
-        <div class="GameDisplay"></div>
-        <div class="box">
-            <div class="loki"></div>
-            <div class="fireball"></div>
-            <div class="ironman"></div>
-        </div>
-        <div class="InteractioPad">
-            <button class="Interaction_Buttons left_btn" onclick="MoveLeft()"></button>
-            <button class="Interaction_Buttons right_btn" onclick="MoveRight()"></button>
-            <button class="restart_btn"onclick="Restart()"></button>
-        </div>
-    </div>
                 <script src='FuncBoxx/AvengersGameFiles/scripts.js'></script>
                 <script>${ js }</script>
             </body>
