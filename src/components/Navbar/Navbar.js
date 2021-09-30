6b93570775bdfeb9463876c3319a921258f3cb2a
nishant0811 <renegade.plays03@gmail.com>
2021-09-30 19:24:10 +0530

    navbar responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {
   Link,
-  BrowserRouter as Router, 
+  BrowserRouter as Router,
   Route,
   Switch,
   Redirect,
@@ -15,14 +15,19 @@ import login from './assets/login.svg'
 import shop from './assets/shop.svg'
 
 export default function Navbar() {
+
+    function toggleNav(){
+        document.getElementById('mobile__links').classList.toggle('active');
+    }
     return (
         <div>
             <div className="navigation">
                 <div className="logo__container">
                 <img src={logo} alt="logo" className="logo" />
+
                 </div>
                 <div className="link">
-                    
+
                 <Link to="/LandingPage">
                 <a href="#">
                     <img src={home} alt="home" />
@@ -39,13 +44,40 @@ export default function Navbar() {
                     <img src={shop} alt="shop" />
                     <p>Shop</p>
                 </a>
-                <Link to="/Login">
+                {/*<Link to="/Login">
                 <a href="#">
                     <img src={login} alt="login"/>
                     <p>Login</p>
                 </a>
-                </Link>
+              </Link>*/}
                 </div>
+                <div className="bars">
+                  <i className="fas fa-bars" onClick={toggleNav}></i>
+                </div>
+            </div>
+            <div className="mobile__links" id="mobile__links">
+              <Link to="/LandingPage">
+              <a href="#">
+                  <img src={home} alt="home" />
+                  <p>Home</p>
+              </a>
+              </Link>
+              <Link to="/AboutPage">
+              <a href="#">
+                  <img src={about} alt="about" />
+                  <p>About</p>
+              </a>
+              </Link>
+              <a href="#">
+                  <img src={shop} alt="shop" />
+                  <p>Shop</p>
+              </a>
+              {/*<Link to="/Login">
+              <a href="#">
+                  <img src={login} alt="login"/>
+                  <p>Login</p>
+              </a>
+            </Link>*/}
             </div>
         </div>
     )
