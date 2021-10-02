81c28cd7ac8b97943fa5c9adfdcaca6796f179fc
HemanthKumar200225 <hemanthkumarbhamidipati@gmail.com>
2021-10-02 21:19:13 +0530

    Added social media links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 
 import './Footer.css'
 
@@ -9,14 +10,23 @@ import linked from "./assets/linked.svg"
 import insta from "./assets/insta.svg"
 
 export default function Footer() {
+
+    const [getInTouchEmail,setGetInTouchEmail] = useState('');
+
+
+    const handleSubmit = () => {
+        alert(`We will reach you out at ${getInTouchEmail}`);
+    }
     return (
         <div>
             <div className="footer">
             <div className="getInTouch">
                 <p>Get in Touch</p>
                 <div className='input'>
-                <input type="text" name="email" />
-                <img src={bell} alt="bell" />
+                <form>
+                    <input type="text" name="email" onChange = { e => setGetInTouchEmail(e.target.value) }/>
+                    <button onClick={handleSubmit}><img src={bell} alt="bell" /></button>
+                </form>
                 </div>
             </div>
             <div className="madeWith">
@@ -24,13 +34,13 @@ export default function Footer() {
             </div>
             <div className="links">
                 <div className="social">
-                <a href="#">
+                <a href="https://www.facebook.com/funcbox.edu" target="_blank">
                     <img src={fb} alt="fb" />
                 </a>
-                <a href="#">
+                <a href="https://www.linkedin.com/company/funcbox" target="_blank">
                     <img src={linked} alt="linked" />
                 </a>
-                <a href="#">
+                <a href="https://www.instagram.com/funcbox_edu/?utm_medium=copy_link" target="_blank">
                     <img src={insta} alt="insta" />
                 </a>
                 </div>
