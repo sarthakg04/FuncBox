d1cf19cd607d8e16c2a1b24cfcc36d6e5cbd68ec
HemanthKumar200225 <hemanthkumarbhamidipati@gmail.com>
2021-10-05 20:55:08 +0530

    Removed Footer for TeamsPage

--- src/components/TeamsPage/TeamsPage.js
+++ src/components/TeamsPage/TeamsPage.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './TeamsPage.css'
 
 import Navbar from '../Navbar/Navbar'
-import Footer from '../Footer/Footer'
+// import Footer from '../Footer/Footer'
 
 export default function TeamsPage() {
     return (
@@ -10,7 +10,7 @@ export default function TeamsPage() {
             <Navbar/>
             <h1 className='Heading' style={{margin: "10px 0 20px 20px"}}>BUILD WITH US</h1>
             <img className="TeamsImg" src="https://ik.imagekit.io/funcboxImages/TeamsPage/teams_DJWcwmp0P.png?updatedAt=1633445213862" alt="teams" width="80%"/>
-            <Footer/>
+            {/* <Footer/> */}
         </div>
     )
 }
