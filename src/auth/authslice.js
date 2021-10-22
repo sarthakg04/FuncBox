af23838b28e00e6e1d916e2578a10c56c861a38a
HemanthKumar200225 <hemanthkumarbhamidipati@gmail.com>
2021-10-23 01:30:01 +0530

    Added UserNavbar

--- src/auth/authslice.js
+++ src/auth/authslice.js
@@ -4,7 +4,7 @@ export const authSlice = createSlice({
   name: "auth",
   initialState: {
     username: "",
-    isAuthenticated: false,
+    isAuthenticated: true,
     token: null,
   },
   reducers: {
--- src/components/UserNavbar/UserNavbar.js
+++ src/components/UserNavbar/UserNavbar.js
@@ -8,7 +8,17 @@ import {
   Redirect,
   useLocation
 } from "react-router-dom";
-import './UserNavbar.css'
+import { useHistory } from "react-router-dom"
+
+import './UserNavbar.css';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
+
+import { setAuth, 
+    // setUser
+ } from '../../auth/authslice';
+import { useDispatch } from 'react-redux';
 
 export default function UserNavbar(props) {
 
@@ -16,11 +26,20 @@ export default function UserNavbar(props) {
     const login = 'https://ik.imagekit.io/funcboxImages/Navbar_assets/login_OS_KcRsPy.png?updatedAt=1633368915926'
     const Username = 'John Doe'
 
-    const [ isAuthenticated, setAuth ] = useState(true)
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const isAuthenticated = useSelector((state) => state.auth.isAuthenticated)
+    // const [ isAuthenticated, setAuth ] = useState())
     const [drop, setDrop] = useState(false)
     
     const dropDown = () => {
         setDrop(!drop)
+    }
+
+    const handleLogout = () => {
+        dispatch(setAuth({ isAuthenticated: false }));
+        history.push("/login");
     }
 
     return (
@@ -29,7 +48,16 @@ export default function UserNavbar(props) {
                 <img src={logo} alt="logo" className="logo" />
                 <div className="optionBox">
                     <div style={{ display: isAuthenticated ? 'block' : 'none'}} className="dropDownOption">
-                        <button onClick={dropDown} ><div className="username">{Username}</div></button>
+                        {/* <button onClick={dropDown} ><div className="username">{Username}</div></button> */}
+                        <div className="dropdown">
+                            <button class="dropbtn">{Username}🔽</button>
+                            <div class="dropdown-content">
+                                <a href="#">Edit Profile</a>
+                                <a href="#">Purchase</a>
+                                <a href="#">Change password</a>
+                                <button onClick={handleLogout}>Logout</button>
+                            </div>
+                        </div>
                     </div>
                     <div style={{ display: isAuthenticated ? 'none' : 'block'}} className="loginOption">
                         <img src={login} alt="logout" />
