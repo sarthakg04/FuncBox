75f185dab9208c637e5c62fe774f9edc18e3f027
Sarthakg04 <sarthakanamika@gmail.com>
2021-10-27 21:39:01 +0530

    Assests added for StatesofMatter

---
+++ public/StatesThatMatterFiles/scripts.js
@@ -0,0 +1,30 @@
+// Frontend functions
+// createGamepad()
+// fillBackground()
+// createInteractionPad()
+
+function createGamepad() {
+    let container = document.createElement("DIV");
+    container.classList.add("container");
+    container.id = "container";
+    document.body.appendChild(container);
+  }
+  
+  function fillBackground() {
+    document.getElementsByClassName("container")[0].style.backgroundImage =
+      "url(https://ik.imagekit.io/funcboxImages/MotuPatlu_assets/background_Cd--9nmmIDu.png?updatedAt=1635330378178)";
+    console.log("background created");
+  }
+  
+  function createInteractionPad() {
+    container.innerHTML += `
+      <div class="speed">
+      <p id="speedoutput">
+      Speed: 
+      <p>
+      </div>
+      <input type="number" id="distance" name="distance" placeholder="Distance (Km)"/>
+      <input type="number" id="time" name="time" placeholder="Time (Hr)"/>
+      <input type="submit" id="orangeBtn" onClick="SpeedOutput()"/>
+      `;
+  }
