13170356426acad40f1430ae5a7cb25e38412fc5
SayarB <sayarbhattacharyya03@gmail.com>
2021-11-08 16:45:20 +0530

    game file paths updated

--- src/gameFilePath.js
+++ src/gameFilePath.js
@@ -1,6 +1,17 @@
 module.exports = {
   filepath: {
-    1: "WhackAMoleFiles",
+    1: "AngryBirds",
+    2: "MissionMarsFiles",
+    3: "MonkeyMultiplierFiles",
+    4: "LetsMakeItRainFiles",
+    5: "BowlingGameFiles",
+    6: "ClockGameFiles",
+    7: "AlienGameFiles",
+    8: "AvengersGameFiles",
+    9: "WhackAMoleFiles",
+    10: "IncrediblesGameFiles",
     11: "ColorFunGameFiles",
+    12: "SolarSystemGameFiles",
+    13: "TemperatureConversionFiles",
   },
 };
