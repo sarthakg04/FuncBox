9f4a6287e8b86620d26e105f58ea7fab6ac5f368
onlykingKD <kaustubhdwivedi1729@gamil.com>
2022-02-03 15:58:01 +0530

    prime logic added

--- public/ShootThePrimesGameFiles/script.js
+++ public/ShootThePrimesGameFiles/script.js
@@ -1,12 +1,13 @@
 /*
     createBackground();
+    createJokers();
+    generateBoxes();
+    generateRandomPrimes();
 */
 
-var arr = [1,2,3,4,5,6,7,8,9];
 Phonex = document.createElement("div");
 Phonex.classList.add("Phonex");
 document.body.appendChild(Phonex);
-
 
 function createBackground() {
   // console.log("createBackground");
@@ -17,88 +18,112 @@ function createBackground() {
       `;
 }
 
-function createJokers(){
-    // console.log("createJokers");
-    Phonex.innerHTML += `
+function createJokers() {
+  // console.log("createJokers");
+  Phonex.innerHTML += `
         <div class="Jokers">
             <img src="./ShootThePrimesGameFiles/assets/Jokers.png" alt="" width="320px" />
         </div>
         `;
 }
 
-
-function generateBoxes(){
-    // console.log("generateBoxes");
-    Phonex.innerHTML += `
-            <div class="box one"></div>
-            <div class="box two"></div>
-            <div class="box three"></div>
-            <div class="box four"></div>
-            <div class="box five"></div>
-            <div class="box six"></div>
-            <div class="box seven"></div>
-            <div class="box eight"></div>
-            <div class="box nine"></div>
+function generateBoxes() {
+  // console.log("generateBoxes");
+  Phonex.innerHTML += `
+            <div class="box one" onClick="checkPrime(1)"></div>
+            <div class="box two" onClick="checkPrime(2)"></div>
+            <div class="box three" onClick="checkPrime(3)"></div>
+            <div class="box four" onClick="checkPrime(4)"></div>
+            <div class="box five" onClick="checkPrime(5)"></div>
+            <div class="box six" onClick="checkPrime(6)"></div>
+            <div class="box seven" onClick="checkPrime(7)"></div>
+            <div class="box eight" onClick="checkPrime(8)"></div>
+            <div class="box nine" onClick="checkPrime(9)"></div>
         `;
 }
 
-var myPrimeNumbers = [];
+var allNumbers = [];
 
-function generateRandomPrimes(){
-    for(let i = 0; i < 4; i++)
-        myPrimeNumbers.push(getRandomPrime([2, 200]));
-    for(let j = 0; j < 5; j++)
-        myPrimeNumbers.push(Math.floor(Math.random() * 200) + 1);
+var primeNumbers = [];
+var nonPrime = [];
 
-    console.log(myPrimeNumbers);
-    myPrimeNumbers = shuffleArray(myPrimeNumbers);
-    console.log("final array is" , myPrimeNumbers);   
+var userPrimes = [];
 
+var countPrime = 0;
 
-    var myBoxes = document.querySelectorAll(".box");
-    for(let i = 0; i < myBoxes.length; i++){
-        myBoxes[i].innerHTML = myPrimeNumbers[i];
+function checkPrime(idx) {
+    // console.log("checkPrime");
+    var allBoxes = document.querySelectorAll(".box");
+    var clickedBox = allBoxes[idx - 1];
+    var clickedNumber = clickedBox.innerHTML;
+    userPrimes.push(clickedNumber);
+    clickedBox.style.display = "none";
+    console.log("userPrimes", userPrimes);
+}
+
+// check if a number is prime or not
+function isPrime(num) {
+    for (var i = 2; i < num/2; i++) {
+        if (num % i === 0) {
+            return false;
+        }
     }
+    return true;
 }
 
 
+function generateRandomPrimes() {
+  for (let i = 0; i < 4; i++) allNumbers.push(getRandomPrime([2, 200]));
+  for (let j = 0; j < 5; j++)
+    allNumbers.push(Math.floor(Math.random() * 200) + 1);
+
+  console.log(allNumbers);
+  allNumbers = shuffleArray(allNumbers);
+  console.log("final array is", allNumbers);
+
+  var myBoxes = document.querySelectorAll(".box");
+  for (let i = 0; i < myBoxes.length; i++) {
+    myBoxes[i].innerHTML = allNumbers[i];
+    if (isPrime(allNumbers[i])){
+        countPrime++; 
+        primeNumbers.push(allNumbers[i]);
+    }
+    else nonPrime.push(allNumbers[i]);
+  }
+  console.log("countPrime is", countPrime);
+}
 
 // Shuffling the array
-
 function shuffleArray(array) {
-    let curId = array.length;
-    // There remain elements to shuffle
-    while (0 !== curId) {
-      // Pick a remaining element
-      let randId = Math.floor(Math.random() * curId);
-      curId -= 1;
-      // Swap it with the current element.
-      let tmp = array[curId];
-      array[curId] = array[randId];
-      array[randId] = tmp;
-    }
-    return array;
+  let curId = array.length;
+  // There remain elements to shuffle
+  while (0 !== curId) {
+    // Pick a remaining element
+    let randId = Math.floor(Math.random() * curId);
+    curId -= 1;
+    // Swap it with the current element.
+    let tmp = array[curId];
+    array[curId] = array[randId];
+    array[randId] = tmp;
   }
-
-
-
+  return array;
+}
 
 // genrating random prime numbers
-
 const range = [100, 1000];
 const getPrimes = (min, max) => {
-   const result = Array(max + 1)
-   .fill(0)
-   .map((_, i) => i);
-   for (let i = 2; i <= Math.sqrt(max + 1); i++) {
-      for (let j = i ** 2; j < max + 1; j += i) delete result[j];
-   }
-   return Object.values(result.slice(min));
+  const result = Array(max + 1)
+    .fill(0)
+    .map((_, i) => i);
+  for (let i = 2; i <= Math.sqrt(max + 1); i++) {
+    for (let j = i ** 2; j < max + 1; j += i) delete result[j];
+  }
+  return Object.values(result.slice(min));
 };
 const getRandomNum = (min, max) => {
-   return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 };
 const getRandomPrime = ([min, max]) => {
-   const primes = getPrimes(min, max);
-   return primes[getRandomNum(0, primes.length - 1)];
+  const primes = getPrimes(min, max);
+  return primes[getRandomNum(0, primes.length - 1)];
 };
