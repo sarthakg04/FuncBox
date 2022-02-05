ad5008387280c485a763a1e24d55b6bb9687bb29
Manav Goyal <manavgoyaltheboss@gmail.com>
2022-02-05 22:11:55 +0530

    Final DeciChemistGame

--- public/DeciChemistGameFiles/script.js
+++ public/DeciChemistGameFiles/script.js
@@ -1,13 +1,22 @@
 // FRONTEND FUNCTIONS at /DeciChemistGame
 // createGamepad();
 // createTopElements();
+// createMiddleElements();
+// createBottomElements();
 // fillBackground();
 // startGame();
 // createStartButton();
 // setSelectedColor();
 // setSelectedOption();
-
-// Give Minimum of 3 Correct answer to Win
+//
+// function generateRandomNumber() {
+// 	let a = Math.floor(1 + Math.random() * 9);
+// 	let b = Math.floor(1 + Math.random() * 9);
+// 	let c = Math.floor(1 + Math.random() * 9);
+// 	ans = `${a}.${b}${c}`;
+// 	chooseRandomColor();
+// }
+//
 
 // Variables
 let ans = 0;
@@ -17,7 +26,9 @@ let selectedColor = "";
 let correctAns = 0;
 
 // Generating a Random color
-function generateRandomColor() {
+function chooseRandomColor() {
+	userAns = 0;
+	document.getElementById("middle-ans").innerHTML = `0 ml`;
 	document.getElementById("middle-beaker-pink").style.height = "0";
 	document.getElementById("middle-beaker-green").style.height = "0";
 	document.getElementById("middle-beaker-red").style.height = "0";
@@ -30,19 +41,7 @@ function generateRandomColor() {
 	document.getElementById(
 		"top-board-ques"
 	).innerHTML = `Add ${ans} ml of ${color} solution`;
-	console.log("Color = ", color);
-}
-
-// Generating a Random Number of 3 digits
-function generateRandomNumber() {
-	userAns = 0;
-	document.getElementById("middle-ans").innerHTML = `0 ml`;
-	let a = Math.floor(1 + Math.random() * 9);
-	let b = Math.floor(1 + Math.random() * 9);
-	let c = Math.floor(1 + Math.random() * 9);
-	ans = `${a}.${b}${c}`;
-	console.log("Answer = ", parseFloat(ans));
-	generateRandomColor();
+	// console.log("Color = ", color);
 }
 
 // Starting Game
@@ -52,6 +51,10 @@ function startGame() {
 			"https://ik.imagekit.io/funcboxImages/DeciChemistGame/tr:w-500/2.png"
 		);
 		document.getElementById("bottom-startBtn").style.display = "block";
+		document.getElementById("top-board-ques").style.visibility = "visible";
+		document.getElementById(
+			"top-board-ques"
+		).innerHTML = `3 correct answer to Win`;
 		const topImgEle = document.getElementsByClassName("top-color-img");
 		for (let i = 0; i < topImgEle.length; i++) {
 			topImgEle[i].style.visibility = "visible";
@@ -62,10 +65,9 @@ function startGame() {
 // Starting Game
 function createStartButton() {
 	document.getElementById("bottom-startBtn").addEventListener("click", () => {
-		console.log("Starting Game");
+		// console.log("Starting Game");
 		generateRandomNumber();
 		document.getElementById("top-board-timer").style.visibility = "visible";
-		document.getElementById("top-board-ques").style.visibility = "visible";
 		document.getElementById("middle-ans").style.visibility = "visible";
 		countDown(60);
 		document.getElementById("bottom-startBtn").style.display = "none";
@@ -84,7 +86,7 @@ function createResetButton() {
 	}
 	document.getElementById("bottom-resetBtn").style.display = "block";
 	document.getElementById("bottom-resetBtn").addEventListener("click", () => {
-		console.log("Restarting Game");
+		// console.log("Restarting Game");
 		document.getElementById("bottom-resetBtn").style.display = "none";
 		correctAns = 0;
 		generateRandomNumber();
@@ -102,9 +104,11 @@ function countDown(num) {
 
 	if (num < 0) {
 		createResetButton();
-		console.log("Correct Answer = ", correctAns);
+		// console.log("Correct Answer = ", correctAns);
 		if (correctAns >= 3) {
-			document.getElementById("top-board-ques").innerHTML = `${correctAns} Correct Answer. You Won!!!`;
+			document.getElementById(
+				"top-board-ques"
+			).innerHTML = `${correctAns} Correct Answer. You Won!!!`;
 		} else {
 			document.getElementById(
 				"top-board-ques"
@@ -124,13 +128,13 @@ function fillColor() {
 	var blue = document.getElementById("middle-beaker-blue");
 
 	if (color == "pink") {
-		pink.style.height = `${userAns * 5}px`;
+		pink.style.height = `${userAns * 7}px`;
 	} else if (color == "green") {
-		green.style.height = `${userAns * 5}px`;
+		green.style.height = `${userAns * 7}px`;
 	} else if (color == "red") {
-		red.style.height = `${userAns * 5}px`;
+		red.style.height = `${userAns * 7}px`;
 	} else if (color == "blue") {
-		blue.style.height = `${userAns * 5}px`;
+		blue.style.height = `${userAns * 7}px`;
 	}
 }
 
@@ -140,7 +144,7 @@ function updateAns(num) {
 	var rounded = Math.round((userAns + Number.EPSILON) * 100) / 100;
 	document.getElementById("middle-ans").innerHTML = `${rounded} ml`;
 	fillColor();
-	console.log("Updated Answer = ", rounded);
+	// console.log("Updated Answer = ", rounded);
 	if (selectedColor != color) {
 		document.getElementById(
 			"top-board-ques"
@@ -170,7 +174,7 @@ function setSelectedColor() {
 	topImgEle.forEach((ele) => {
 		ele.onclick = () => {
 			selectedColor = ele.alt;
-			console.log("Color Selected = ", ele.alt);
+			// console.log("Color Selected = ", ele.alt);
 		};
 	});
 }
@@ -183,7 +187,7 @@ function setSelectedOption() {
 			let numb = ele.innerText.match(/\d/g);
 			// console.log(numb);
 			let numGenerated = `${numb[0]}.${numb[1]}${numb[2]}`;
-			console.log("Option Selected = ", parseFloat(numGenerated));
+			// console.log("Option Selected = ", parseFloat(numGenerated));
 			updateAns(parseFloat(numGenerated));
 		};
 	});
@@ -200,51 +204,108 @@ function createGamepad() {
 // Create HTML
 function createTopElements() {
 	const mainContainer = document.getElementById("GamePad");
-	mainContainer.innerHTML = `
-    <div id="top">
-      <div id="top-board">
-        <div id="top-board-timer">60</div>
-        <div id="top-board-ques"></div>
-      </div>
-      <div id="top-color">
-        <div id="top-color-pink">
-          <img src="https://ik.imagekit.io/funcboxImages/DeciChemistGame/tr:w-500/pink.png" alt="pink" height="100px"
-            class="top-color-img">
-        </div>
-        <div id="top-color-green">
-          <img src="https://ik.imagekit.io/funcboxImages/DeciChemistGame/tr:w-500/green.png" alt="green" height="100px"
-            class="top-color-img">
-        </div>
-        <div id="top-color-red">
-          <img src="https://ik.imagekit.io/funcboxImages/DeciChemistGame/tr:w-500/red.png" alt="red" height="100px"
-            class="top-color-img">
-        </div>
-        <div id="top-color-blue">
-          <img src="https://ik.imagekit.io/funcboxImages/DeciChemistGame/tr:w-500/blue.png" alt="blue" height="100px"
-            class="top-color-img">
-        </div>
-      </div>
-    </div>
+	var top = document.createElement("div");
+	top.id = "top";
+	var topBoard = document.createElement("div");
+	topBoard.id = "top-board";
+	var topBoardTimer = document.createElement("div");
+	topBoardTimer.id = "top-board-timer";
+	var topBoardQues = document.createElement("div");
+	topBoardQues.id = "top-board-ques";
+	topBoard.appendChild(topBoardTimer);
+	topBoard.appendChild(topBoardQues);
+	top.appendChild(topBoard);
+	var topColor = document.createElement("div");
+	topColor.id = "top-color";
+	var topColorPink = document.createElement("div");
+	topColorPink.id = "top-color-pink";
+	topColorPink.innerHTML = `<img src="https://ik.imagekit.io/funcboxImages/DeciChemistGame/tr:w-500/pink.png" alt="pink" height="100px"
+            class="top-color-img">`;
+	var topColorGreen = document.createElement("div");
+	topColorGreen.id = "top-color-green";
+	topColorGreen.innerHTML = `<img src="https://ik.imagekit.io/funcboxImages/DeciChemistGame/tr:w-500/green.png" alt="green" height="100px"
+            class="top-color-img">`;
+	var topColorRed = document.createElement("div");
+	topColorRed.id = "top-color-red";
+	topColorRed.innerHTML = `<img src="https://ik.imagekit.io/funcboxImages/DeciChemistGame/tr:w-500/red.png" alt="red" height="100px"
+            class="top-color-img">`;
+	var topColorBlue = document.createElement("div");
+	topColorBlue.id = "top-color-blue";
+	topColorBlue.innerHTML = `<img src="https://ik.imagekit.io/funcboxImages/DeciChemistGame/tr:w-500/blue.png" alt="blue" height="100px"
+            class="top-color-img">`;
+	topColor.appendChild(topColorPink);
+	topColor.appendChild(topColorGreen);
+	topColor.appendChild(topColorRed);
+	topColor.appendChild(topColorBlue);
+	top.appendChild(topColor);
+	mainContainer.appendChild(top);
+}
 
-    <div id="middle">
-      <div id="middle-beaker">
-        <div id="middle-beaker-pink"></div>
-        <div id="middle-beaker-green"></div>
-        <div id="middle-beaker-red"></div>
-        <div id="middle-beaker-blue"></div>
-      </div>
-      <div id="middle-ans">0 ml</div>
-    </div>
+function createMiddleElements() {
+	const mainContainer = document.getElementById("GamePad");
+	var middle = document.createElement("div");
+	middle.id = "middle";
+	var middleBeaker = document.createElement("div");
+	middleBeaker.id = "middle-beaker";
+	var middleBeakerPink = document.createElement("div");
+	middleBeakerPink.id = "middle-beaker-pink";
+	var middleBeakerGreen = document.createElement("div");
+	middleBeakerGreen.id = "middle-beaker-green";
+	var middleBeakerRed = document.createElement("div");
+	middleBeakerRed.id = "middle-beaker-red";
+	var middleBeakerBlue = document.createElement("div");
+	middleBeakerBlue.id = "middle-beaker-blue";
+	middleBeaker.appendChild(middleBeakerPink);
+	middleBeaker.appendChild(middleBeakerGreen);
+	middleBeaker.appendChild(middleBeakerRed);
+	middleBeaker.appendChild(middleBeakerBlue);
+	var middleAns = document.createElement("div");
+	middleAns.id = "middle-ans";
+	middleAns.innerText = "0 ml";
+	middle.appendChild(middleBeaker);
+	middle.appendChild(middleAns);
+	mainContainer.appendChild(middle);
+}
 
-    <div id="bottom">
-      <div class="btn" id="bottom-startBtn">Get Started</div>
-      <div class="btn btn-option" id="bottom-optionBtn1">1.0 ml</div>
-      <div class="btn btn-option" id="bottom-optionBtn2">0.5 ml</div>
-      <div class="btn btn-option" id="bottom-optionBtn3">0.1 ml</div>
-      <div class="btn btn-option" id="bottom-optionBtn4">0.01 ml</div>
-      <div class="btn" id="bottom-resetBtn">Replay</div>
-    </div>
-  `;
+function createBottomElements() {
+	const mainContainer = document.getElementById("GamePad");
+	var bottom = document.createElement("div");
+	bottom.id = "bottom";
+	var bottomStartBtn = document.createElement("div");
+	bottomStartBtn.id = "bottom-startBtn";
+	bottomStartBtn.classList.add("btn");
+	bottomStartBtn.innerText = "Get Started";
+	var bottomOptionBtn1 = document.createElement("div");
+	bottomOptionBtn1.id = "bottom-optionBtn1";
+	bottomOptionBtn1.classList.add("btn");
+	bottomOptionBtn1.classList.add("btn-option");
+	bottomOptionBtn1.innerText = "1.0 ml";
+	var bottomOptionBtn2 = document.createElement("div");
+	bottomOptionBtn2.id = "bottom-optionBtn2";
+	bottomOptionBtn2.classList.add("btn");
+	bottomOptionBtn2.classList.add("btn-option");
+	bottomOptionBtn2.innerText = "0.5 ml";
+	var bottomOptionBtn3 = document.createElement("div");
+	bottomOptionBtn3.id = "bottom-optionBtn3";
+	bottomOptionBtn3.classList.add("btn");
+	bottomOptionBtn3.classList.add("btn-option");
+	bottomOptionBtn3.innerText = "0.1 ml";
+	var bottomOptionBtn4 = document.createElement("div");
+	bottomOptionBtn4.id = "bottom-optionBtn4";
+	bottomOptionBtn4.classList.add("btn");
+	bottomOptionBtn4.classList.add("btn-option");
+	bottomOptionBtn4.innerText = "0.01 ml";
+	var bottomResetBtn = document.createElement("div");
+	bottomResetBtn.id = "bottom-resetBtn";
+	bottomResetBtn.classList.add("btn");
+	bottomResetBtn.innerText = "Replay";
+	bottom.appendChild(bottomStartBtn);
+	bottom.appendChild(bottomOptionBtn1);
+	bottom.appendChild(bottomOptionBtn2);
+	bottom.appendChild(bottomOptionBtn3);
+	bottom.appendChild(bottomOptionBtn4);
+	bottom.appendChild(bottomResetBtn);
+	mainContainer.appendChild(bottom);
 }
 
 // Fill background
