c9aacf37f5533ed53ba57d8178b576295a12f9a3
HemanthKumar200225 <hemanthkumarbhamidipati@gmail.com>
2022-02-08 08:50:54 +0530

    Debugged the game LetsMakeItRain

--- src/components/Games/LetsMakeItRain/LetsMakeItRain.js
+++ src/components/Games/LetsMakeItRain/LetsMakeItRain.js
@@ -1,9 +1,10 @@
 import React from "react";
 import GameContainer from "../GameContainer/GameContainer";
-export default function MakeItRainGame() {
+export default function LetsMakeItRain() {
   return (
     <div>
       <GameContainer gid={4} />
     </div>
   );
 }
+
