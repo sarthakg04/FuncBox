5508304605d649e76daf4eba78fe8598d339939b
Manav Goyal <manavgoyaltheboss@gmail.com>
2022-02-11 17:51:54 +0530

    Carousel & About page image fixed

--- src/components/LandingPage/Carousel/CarouselComp.js
+++ src/components/LandingPage/Carousel/CarouselComp.js
@@ -1,66 +1,71 @@
 import React, { useState } from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import "./Carousel.css";
 import { Cards } from "./CarouselData";
+import CarouselItem from "./CarouselItem";
 
 function CarouselComp() {
-	var settings = {
-		dots: true,
-		dotsClass: "slick-dots",
-		infinite: true,
-		// autoplay: true,
-		// autoplaySpeed: 5000,
-		speed: 500,
-		slidesToShow: 4,
-		slidesToScroll: 1,
-		focusOnSelect: true,
-		centerMode: true,
-		// centerPadding: "50px",
-		cssEase: "linear",
-		responsive: [
-			{
-				breakpoint: 1240,
-				settings: {
-					slidesToShow: 4,
-					// centerMode: true,
-					// centerPadding: "100px",
-				},
-			},
-			{
-				breakpoint: 1000,
-				settings: {
-					slidesToShow: 3,
-				},
-			},
-			{
-				breakpoint: 768,
-				settings: {
-					slidesToShow: 2,
-				},
-			},
-			{
-				breakpoint: 550,
-				settings: {
-					slidesToShow: 1,
-				},
-			},
-		],
+	// Use State Variable
+	const [leftLeftSrc, setLeftLeftSrc] = useState(0);
+	const [leftSrc, setLeftSrc] = useState(1);
+	const [centerSrc, setCenterSrc] = useState(2);
+	const [rightSrc, setRightSrc] = useState(3);
+	const [rightRightSrc, setRightRightSrc] = useState(4);
+
+	let cssProperties = {};
+
+	// Functions
+	const prevSlide = () => {
+		// cssProperties["--moveImg"] = "-10rem";
+		setLeftLeftSrc(leftLeftSrc === 0 ? Cards.length - 1 : leftLeftSrc - 1);
+		setLeftSrc(leftSrc === 0 ? Cards.length - 1 : leftSrc - 1);
+		setCenterSrc(centerSrc === 0 ? Cards.length - 1 : centerSrc - 1);
+		setRightSrc(rightSrc === 0 ? Cards.length - 1 : rightSrc - 1);
+		setRightRightSrc(
+			rightRightSrc === 0 ? Cards.length - 1 : rightRightSrc - 1
+		);
+	};
+
+	const nextSlide = () => {
+		// cssProperties["--moveImg"] = "10rem";
+		setLeftLeftSrc(leftLeftSrc === Cards.length - 1 ? 0 : leftLeftSrc + 1);
+		setLeftSrc(leftSrc === Cards.length - 1 ? 0 : leftSrc + 1);
+		setCenterSrc(centerSrc === Cards.length - 1 ? 0 : centerSrc + 1);
+		setRightSrc(rightSrc === Cards.length - 1 ? 0 : rightSrc + 1);
+		setRightRightSrc(
+			rightRightSrc === Cards.length - 1 ? 0 : rightRightSrc + 1
+		);
 	};
 
 	return (
 		<div className="carouselSlider">
-			<div className="carouselSliderImages">
-				<Slider {...settings}>
-					{Cards.map((val, idx) => {
-						return (
-							<div key={idx} className="carouselSliderImagesDiv">
-								<img src={val.imgSrc} alt="" height="300px" />
-							</div>
-						);
-					})}
-				</Slider>
+			<div className="carouselSliderLeft" onClick={prevSlide}>
+				<IoIosArrowBack size={50} />
+			</div>
+
+			<div className="carouselSliderImages" style={cssProperties}>
+				{Cards.map((val, idx) => {
+					return (
+						<div
+							className={idx === centerSrc ? "active slide" : "slide"}
+							key={idx}
+						>
+							{idx === centerSrc && (
+								<CarouselItem
+									leftLeftImgSrc={Cards[leftLeftSrc].imgSrc}
+									leftImgSrc={Cards[leftSrc].imgSrc}
+									centerImgSrc={Cards[centerSrc].imgSrc}
+									rightImgSrc={Cards[rightSrc].imgSrc}
+									rightRightImgSrc={Cards[rightRightSrc].imgSrc}
+								/>
+							)}
+						</div>
+					);
+				})}
+			</div>
+
+			<div className="carouselSliderRight" onClick={nextSlide}>
+				<IoIosArrowForward size={50} />
 			</div>
 		</div>
 	);
