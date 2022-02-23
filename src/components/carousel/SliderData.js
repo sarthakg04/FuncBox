0f4d9a6bc325f8275ad1ecfb5044a50c6a5b69fe
Deepak Raj Gupta <deepak65521@gmail.com>
2022-02-23 19:21:48 +0530

    added carousel  and fixed percentage calculator game

---
+++ src/components/carousel/SliderData.js
@@ -0,0 +1,20 @@
+import img1 from "../carousel/Images/1.png";
+import img2 from "../carousel/Images/2.png";
+import img3 from "../carousel/Images/3.png";
+export const SLIDER_DATA = [
+  {
+    id: 1,
+    image: img1,
+    text: "Teach your kid on how he/she can become an Inventor. How to bring his ideas to life and out of his books. We at FuncBox aim to develop an ecosystem for your kids to become a problem solver.",
+  },
+  {
+    id: 2,
+    image: img2,
+    text: "Stand Out from all the other Educational Institutes. Introduce the concepts of coding to your students at an Early Age. We help schools build an ecosystem to help kids implement the concepts taught in the classroom with the help of programming.",
+  },
+  {
+    id: 3,
+    image: img3,
+    text: "FuncBox provides an exclusive websites for your institute. So that students don't feel that they are going away from schools. All the Boxes which we will deliver to you will be exclusively made for your institute and with a personalized addition of Apps according to the curriculum you follow.",
+  },
+];
