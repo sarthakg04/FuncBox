71f36cf4c6d14d44034ffd847f76be9f492929b9
Chandan Thakur <thakurchandan4562@gmail.com>
2022-02-25 21:18:29 +0545

    Background themes added in HitIt game

--- public/HitItFiles/script.js
+++ public/HitItFiles/script.js
@@ -1,6 +1,7 @@
 //Fontend Functions
 // createGamepad()
-// fillBackground()
+// //Either background is cricket or football
+// fillBackground("cricket")
 // createScore();
 // let timeRemaining = 240;
 // createQuestionsContainer()
@@ -10,21 +11,21 @@
 //   if(qType == 'even'){
 //     if(number%2==0){
 //       score+=1;
-//
+
 //     }
 //     else{
 //       score-=1;
-//
+
 //     }
 //   }
 //   else {
 //     if(number%2 !=0){
 //       score+=1;
-//
+
 //     }
 //     else{
 //       score-=1;
-//
+
 //     }
 //   }
 // }
@@ -36,7 +37,7 @@ function createGamepad(){
   document.body.appendChild(gamePadd);
   gamePad = document.getElementById('GamePad')
 }
-
+var backImage="cricket";
 function createScore(){
   document.getElementsByClassName('GamePad')[0].innerHTML +=`
   <div class="score">
@@ -45,8 +46,10 @@ function createScore(){
   `;
 }
 
-function fillBackground(){
-  document.getElementsByClassName('GamePad')[0].style.backgroundImage =  'url(./HitItFiles/assets/background.png)';
+function fillBackground(image){
+  backImage=image;
+  document.getElementsByClassName('GamePad')[0].style.backgroundImage =  `url(./HitItFiles/assets/${backImage}background.png)`;
+  document.getElementsByClassName('GamePad')[0].innerHTML +=`<div id="ball"></div>`;
 }
 
 function createQuestionsContainer(){
@@ -66,9 +69,10 @@ function createInteractionPad(){
 
   <div class="restart__button" id='resBtn'>
     <p id="fscore">Score : 10</p>
-    <button type="button" name="button" class="action__btn" onclick="restart()">Restart</button>
+    <button type="button" name="button" class="action__btn " onclick="restart()">Restart</button>
   </div>
   `;
+  
 }
 
 function initializeGameEngine(){
@@ -81,10 +85,11 @@ function initializeGameEngine(){
     <p id="q"></p>
   </div>
 
-  <div class="options">
+  <div class="options" id="oppo">
 
   </div>
   `;
+  
 }
 
 
@@ -97,6 +102,7 @@ let score = 0;
 
 function startGame(){
   remaining = timeRemaining
+  
   time = setInterval(()=>{
     if(remaining != 0){
 
@@ -124,6 +130,7 @@ function startGame(){
   document.getElementById('playBtn').classList.remove('active');
   document.getElementById('container').classList.toggle('active');
   generateQustion();
+  ballTextColor();
 }
 
 function endGame(){
@@ -131,8 +138,23 @@ function endGame(){
   document.getElementById('fscore').innerHTML = 'Score : '+ score;
   document.getElementById('score').innerHTML = 'END';
   document.getElementById('resBtn').classList.add('active');
+  // document.getElementById("ball").style.display="none";
+  document.getElementById("ball").innerHTML="";
+}
+function createBalls(){
+  document.getElementById("ball").innerHTML=`<div id="ball1"><img src="./HitItFiles/assets/${backImage}ball.png"></div>
+  <div id="ball2"><img src="./HitItFiles/assets/${backImage}ball.png"></div>
+  <div id="ball3"><img src="./HitItFiles/assets/${backImage}ball.png"></div>`; 
 }
 
+function ballTextColor(){
+  if (backImage=="cricket"){
+    document.getElementById("oppo").classList.remove("opption");
+  }
+  else if(backImage=="football"){
+    document.getElementById("oppo").classList.add("opption");
+  }
+}
 
 function generateQustion(){
   type = Math.floor(Math.random() * (1 - 0 + 1) + 0);
@@ -180,13 +202,13 @@ function generateQustion(){
     let index = Math.floor(Math.random() * ((ops.length-1) - 0 + 1) + 0)
     document.getElementsByClassName('options')[0].innerHTML += `
     <div class="option" onclick = 'validate(${ops[index]})'>
-      <p>${ops[index]}</p>
+      <p id="opColor">${ops[index]}</p>
     </div>
     `;
 
     ops.splice(index,1);
   }
-
+  createBalls()
 }
 
 
@@ -198,6 +220,7 @@ function validate(num){
   else{
     typee = 'odd'
   }
+  
   checkSelected(num,typee);
   document.getElementById('score').innerHTML = score
   generateQustion();
